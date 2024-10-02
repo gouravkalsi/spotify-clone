87437be329a3d73935dbf8dad7db46a0aee7fb94
gouravkalsi <kalsigourav86280@gmail.com>
2024-10-03 02:25:55 +0530

    bf

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <PlayerContextprovider>
-    <app/>
+  <App/>
     </PlayerContextprovider>
     </BrowserRouter>
   </StrictMode>,
